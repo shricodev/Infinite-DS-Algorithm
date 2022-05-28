d3b8d43258b1f5c844e39913d9b355f6d27fa3b3
r3alix01 <coveringguy0123@gmail.com>
2022-05-28 10:33:34 +0545

    added few more.

---
+++ Recursions/SubsequencesOfString.java
@@ -0,0 +1,18 @@
+public class SubsequencesOfString {
+    public static void main(String[] args) {
+        subsequencesString("pabloescobarisamotherfucker", 0, "");
+    }
+
+    static void subsequencesString(String word, int index, String newString) {
+
+        if(index == word.length()) {
+            System.out.println(newString);
+            return;
+        }
+
+        char currentCharacter = word.charAt(index);
+
+        subsequencesString(word, index + 1, newString + currentCharacter);
+        subsequencesString(word, index + 1, newString);
+    }
+}

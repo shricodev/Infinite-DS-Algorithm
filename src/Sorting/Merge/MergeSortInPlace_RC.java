15df6e83c9306e64005684ec7e24f064a7066886
r3alix01 <coveringguy0123@gmail.com>
2022-07-17 14:05:41 +0545

    finally added all the important sortig algo with codes.

--- src/Sorting/Merge/MergeSortInPlace_RC.java
+++ src/Sorting/Merge/MergeSortInPlace_RC.java
@@ -1,5 +1,68 @@
 package Sorting.Merge;
 
+import java.util.Arrays;
+
 public class MergeSortInPlace_RC {
-    
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 3, 2, 1, -1 };
+        mergeSortInPlace(arr, 0, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void mergeSortInPlace(int[] arr, int strt, int end) {
+        
+        // base case
+        if (end - strt == 1) {
+            return;
+        }
+
+        int mid = strt + (end - strt) / 2;
+
+        // left
+        mergeSortInPlace(arr, strt, mid);
+        // right
+        mergeSortInPlace(arr, mid, end);
+
+        mergeInPlace(arr, strt, mid, end);
+
+    }
+
+    static void mergeInPlace(int arr[], int strt, int mid, int end) {
+        // temp array.
+        int[] mix = new int[end - strt];
+
+        int leftPointer = strt;
+        int rightPointer = mid;
+        int mixPointer = 0;
+
+        while (leftPointer < mid && rightPointer < end) {
+            if (arr[leftPointer] < arr[rightPointer]) {
+                mix[mixPointer] = arr[leftPointer];
+                leftPointer++;
+            } else {
+                mix[mixPointer] = arr[rightPointer];
+                rightPointer++;
+            }
+            mixPointer++;
+        }
+
+        while (leftPointer < mid) {
+            mix[mixPointer] = arr[leftPointer];
+            leftPointer++;
+            mixPointer++;
+        }
+
+        while (rightPointer < end) {
+            mix[mixPointer] = arr[rightPointer];
+            rightPointer++;
+            mixPointer++;
+        }
+
+        // now we have the sorted array in mix. so use a for loop to add the mix elem in
+        // arr.
+        for (int i = 0; i < mix.length; i++) {
+            arr[strt + i] = mix[i];
+        }
+
+    }
 }
---
+++ src/Sorting/Quick/QuickSort_RC.java
@@ -0,0 +1,73 @@
+package Sorting.Quick;
+
+import java.util.Arrays;
+
+// this is not a stable sorting algorithm.
+// this is a in-place sorting algo and don use any extra memory space.
+// avg/best time complexity: O(Nlogn)
+// worst time complexity: O(N^2)
+
+// space complexity: O(1)
+
+public class QuickSort_RC {
+    public static void main(String[] args) {
+        int arr[] = {2, 5, 3, -3, -1, -22, 0, 8, 22, 9};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void quickSort(int[] arr, int start, int end) {
+        if (start >= end) return;
+
+        int partition = partition(arr, start, end);
+
+        // sort the left portion of the partition.
+        quickSort(arr, start, partition - 1);
+        // sort the right portion of the partition.
+        quickSort(arr, partition + 1, end);
+    }
+
+    private static int partition(int[] arr, int start, int end) {
+        int pivot = arr[start];
+        int count = 0;
+
+        // placing the pivot in the correct position.
+        for (int i = start + 1; i <= end; i++) {
+            if (arr[i] <= pivot) {
+                count++;
+            }
+        }
+
+        int pivotIndx = start + count;
+        swap(arr, pivotIndx, start);
+
+        int i = start;
+        int j = end;
+
+        while (i < pivotIndx && j > pivotIndx) {
+            while (arr[i] < pivot) {
+                i++;
+            }
+
+            while (arr[j] > pivot) {
+                j--;
+            }
+
+            if (i < pivotIndx && j > pivotIndx) {
+                // swap(arr, i, j);
+                // i++;
+                // j--;
+                /* OR */
+                swap(arr, i++, j--);
+            }
+        }
+        return pivotIndx;
+    }
+
+    private static void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+}

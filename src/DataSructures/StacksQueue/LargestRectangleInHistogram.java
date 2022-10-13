2119d8745d8b1f85dd406f6d1d2eadbd640171f8
YuShx01 <coveringguy0123@gmail.com>
2022-10-13 09:30:54 +0545

    added the largest rectangle in the histogram code, but it seem to not work for all the test cases, need to modify in the future. NOT the best code, just the bruteforce approach

---
+++ src/DataSructures/StacksQueue/LargestRectangleInHistogram.java
@@ -0,0 +1,54 @@
+
+public class LargestRectangleInHistogram {
+
+    public static void main(String[] args) {
+        int[] heights = { 2, 3 };
+        // System.out.println(findArea(heights));
+    }
+
+    // BRUTE APPROACH
+    // dont know what is wrong with the code.
+    // it does not pass all the test cases of any online judge.
+    // time complexity; O(n^2)
+    // static int findArea(int[] heights) {
+    //     int n = heights.length;
+    //     int maxi = 0;
+
+    //     for (int i = 0; i < n; i++) {
+    //         if (i < n - 1 && heights[i] != heights[i + 1]) {
+    //             break;
+    //         } else {
+    //             if (i == n - 1) {
+    //                 return heights[i] * n;
+    //             }
+    //         }
+    //     }
+
+    //     for (int i = 0; i < n; i++) {
+    //         int leftSmallInd = i;
+    //         int rightSmallInd = i;
+
+    //         // for finding the left just smallest element.
+    //         for (int j = i; j >= 0; j--) {
+    //             if (heights[j] < heights[i]) {
+    //                 // this is the boundary of the left smaller index that is the + 1.
+    //                 leftSmallInd = j + 1;
+    //                 break;
+    //             }
+    //         }
+
+    //         // for finding the right just smallest element.
+    //         for (int j = i; j < n; j++) {
+    //             if (heights[j] < heights[i]) {
+    //                 // this is the boundary of the right smaller index that is the - 1.
+    //                 rightSmallInd = j - 1;
+    //                 break;
+    //             }
+    //         }
+
+    //         maxi = Math.max(maxi, (rightSmallInd - leftSmallInd + 1) * heights[i]);
+    //     }
+
+    //     return maxi;
+    // }
+}

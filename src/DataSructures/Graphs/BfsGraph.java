711c7a9993d1e8c2293cd4757ea627a02eebe9b4
YuShx01 <coveringguy0123@gmail.com>
2022-10-15 11:55:00 +0545

    added the bfs traversal code to the graph folder.

---
+++ src/DataSructures/Graphs/BfsGraph.java
@@ -0,0 +1,54 @@
+package DataSructures.Graphs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * bfsGraph
+ */
+public class BfsGraph {
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        int v = 5;
+        for (int i = 0; i < v; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        adj.get(0).add(0);
+        adj.get(0).add(1);
+        adj.get(1).add(0);
+        adj.get(1).add(2);
+        adj.get(2).add(0);
+        adj.get(2).add(3);
+        adj.get(3).add(2);
+        adj.get(3).add(4);
+        System.out.println(bfsOfGraph(v, adj));
+    }
+
+    public static ArrayList<Integer> bfsOfGraph(int v, ArrayList<ArrayList<Integer>> adj) {
+
+        ArrayList<Integer> bfs = new ArrayList<Integer>();
+        boolean[] vis = new boolean[v];
+        Queue<Integer> q = new LinkedList<>();
+        
+        q.add(0);
+        vis[0] = true;
+        
+        while (!q.isEmpty()) {
+            // .poll() method returns and removes the element so no need to pop.
+            Integer node = q.poll();
+            bfs.add(node);
+            
+            for(Integer it : adj.get(node)) {
+                if (!vis[it]) {
+                    q.add(it);
+                    vis[it] = true;
+                }
+            }
+        }
+        return bfs;
+    }
+}

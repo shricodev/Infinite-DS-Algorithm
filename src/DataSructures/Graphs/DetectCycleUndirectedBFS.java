2b83e05c5b74324d666567ff43b7cfd2d53b3829
YuShx01 <coveringguy0123@gmail.com>
2022-10-17 12:08:59 +0545

    added the code for the cycle detection in the undirected graph using BFS

---
+++ src/DataSructures/Graphs/DetectCycleUndirectedBFS.java
@@ -0,0 +1,61 @@
+package DataSructures.Graphs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Pair {
+    int node;
+    int parent;
+
+    public Pair(int node, int parent) {
+        this.node = node;
+        this.parent = parent;
+    }
+}
+
+public class DetectCycleUndirectedBFS {
+
+    public boolean isCycle(int v, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        boolean[] vis = new boolean[v + 1];
+        Arrays.fill(vis, false);
+        // this handles the case for the connected components.
+        // NOTE: we say a graph has a cycle if any of the components has a cycle.
+        for (int i = 0; i < v; i++) {
+            if (vis[i] == false) {
+                if (bfs(v, adj, i, vis) == true)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean bfs(int v, ArrayList<ArrayList<Integer>> adj, int src, boolean[] vis) {
+
+        Queue<Pair> q = new LinkedList<>();
+        // we adding the parent of the first node -1.
+        q.add(new Pair(src, -1));
+        vis[src] = true;
+
+        while (!q.isEmpty()) {
+            int nodeVal = q.peek().node;
+            int parent = q.peek().parent;
+            q.remove();
+
+            for (Integer adjNode : adj.get(nodeVal)) {
+                if (!vis[adjNode]) {
+                    q.add(new Pair(adjNode, nodeVal));
+                    vis[adjNode] = true;
+                
+                    // this handles for the true case, if the adjacent node is true and the adjnode is not parent
+                    // then it means that the first bfs call has made the node true, so return true.
+                } else if (parent != adjNode) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}

b61ec53cfb5f88c52b9c89cd4c7ae86c6ccffa43
YuShx01 <coveringguy0123@gmail.com>
2022-10-19 21:01:52 +0545

    added the code to the detect cycle in a directed graph using DFS using vis and pathVis array using the striver approach.

---
+++ src/DataSructures/Graphs/DetectCycleDirectedDFS.java
@@ -0,0 +1,44 @@
+package DataSructures.Graphs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DetectCycleDirectedDFS {
+    public static void main(String[] args) {
+        
+    }
+
+    // time complexity; O(v + e)
+    // space complexity; O(2n)
+    public boolean isCycle(int v, ArrayList<ArrayList<Integer>> adj) {
+
+        boolean[] vis = new boolean[v];
+        boolean[] pathVis = new boolean[v];
+        // these two lines not needed.
+        Arrays.fill(vis, false);
+        Arrays.fill(pathVis, false);
+
+        for (int i = 0; i < v; i++) {
+            if (!vis[i]) {
+                if (dfs(vis, pathVis, adj, i) == true) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(boolean[] vis, boolean[] pathVis, ArrayList<ArrayList<Integer>> adj, int node) {
+
+        vis[node] = true;
+        pathVis[node] = true;
+
+        for (Integer it : adj.get(node)) {
+            if (!vis[it]) {
+                if (dfs(vis, pathVis, adj, it) == true) return true;
+            } else if (vis[it] == true && pathVis[it] == true) {
+                return true;
+            }
+        }
+        pathVis[node] = false;
+        return false;
+    }
+}

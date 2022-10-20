b6d9728d009fdd837c7365b36cdcd98ee762a358
YuShx01 <coveringguy0123@gmail.com>
2022-10-20 21:11:38 +0545

    added the kahn's algorithm (BFS topological sorting)

---
+++ src/DataSructures/Graphs/KahnAlgorithmBFS.java
@@ -0,0 +1,45 @@
+package DataSructures.Graphs;
+
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class KahnAlgorithmBFS {
+
+    // time complexity; O(v + e)
+    // space complexity; O(3v)
+    public static int[] topoSort(int v, ArrayList<ArrayList<Integer>> adj) {
+
+        int[] indegree = new int[v];
+
+        // for finding the indegree of the nodes.
+        for (int i = 0; i < v; i++) {
+            for (int it : adj.get(i)) {
+                indegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        // since this is a directed acyclic graph , so there is for sure a node with no
+        // indegree.
+        for (int i = 0; i < v; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+        int[] topo = new int[v];
+        int i = 0;
+        while (!q.isEmpty()) {
+            int node = q.peek();
+            q.remove();
+            topo[i] = node;
+            i++;
+            for (int it : adj.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.add(it);
+            }
+        }
+        return topo;
+    }
+}
---
+++ src/ImpAlgorithms/KahnAlgorithmBFS.java
@@ -0,0 +1,43 @@
+package ImpAlgorithms;
+
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class KahnAlgorithmBFS {
+
+    public static int[] topoSort(int v, ArrayList<ArrayList<Integer>> adj) {
+
+        int[] indegree = new int[v];
+
+        // for finding the indegree of the nodes.
+        for (int i = 0; i < v; i++) {
+            for (int it : adj.get(i)) {
+                indegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        // since this is a directed acyclic graph , so there is for sure a node with no
+        // indegree.
+        for (int i = 0; i < v; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+        int[] topo = new int[v];
+        int i = 0;
+        while (!q.isEmpty()) {
+            int node = q.peek();
+            q.remove();
+            topo[i] = node;
+            i++;
+            for (int it : adj.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.add(it);
+            }
+        }
+        return topo;
+    }
+}

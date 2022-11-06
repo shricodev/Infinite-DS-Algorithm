24c570fcefd11c717d1eda57a5e9ba5cbcec4e19
YuShx01 <coveringguy0123@gmail.com>
2022-11-06 21:32:58 +0545

    added teh code for the identical trees

---
+++ src/DataSructures/Trees/IdenticalTrees.java
@@ -0,0 +1,45 @@
+package DataSructures.Trees;
+
+// import java.util.ArrayList;
+// import java.util.Stack;
+
+public class IdenticalTrees {
+    
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // * brute soln, that came to my mind 
+        // * this code works but when we encounter null as node val, it fails. Will modify this in future.
+        // if (p == null && q == null) return true;
+        // ArrayList<Integer> guide = new ArrayList<>();
+        // Stack<TreeNode> st = new Stack<>();  
+        // st.add(p);
+        // for the root node p
+        // while (!st.isEmpty()) {
+        //     TreeNode node = st.pop();
+        //     guide.add(node.val);
+        //     if (node.right != null) st.add(node.right);
+        //     if (node.left != null) st.add(node.left);
+        // }
+
+        // st.add(q);
+        // int counter = 0;
+        // while (!st.isEmpty()) {
+        //     TreeNode node = st.pop();
+        //     if (guide.get(counter) != node.val) return false;
+        //     if (node.right != null) st.add(node.right);
+        //     if (node.left != null) st.add(node.left);
+        //     counter++;
+        // }
+
+        // return true;
+        
+        // * short and crisp code, learned from striver.
+        // if either of one is null and other is not null
+        if (p == null && q != null || p != null && q == null) {
+            return false;
+        }
+        // if both are null, then they are same.
+        if (p == null && q == null) return true;
+
+        return (p.val == q.val) && isSameTree(p.left,  q.left) && isSameTree(p.right,  q.right);
+    }
+}

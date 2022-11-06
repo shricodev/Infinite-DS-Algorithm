76366515ba75ae793f555946b44d87794a1296dd
YuShx01 <coveringguy0123@gmail.com>
2022-11-06 09:11:37 +0545

    added the code for the maxm depth of the binary tree using the recursion and the iteration approach

---
+++ src/DataSructures/Trees/MaximumDepth.java
@@ -0,0 +1,33 @@
+package DataSructures.Trees;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaximumDepth {
+    // time complexity: O(n)
+    // space complexity: O(n)
+    public int maxDepthRec(TreeNode node) {
+        if (node == null) return 0;
+        int maxLeft = maxDepthRec(node.left);
+        int maxRight = maxDepthRec(node.right);
+        return 1 + Math.max(maxLeft, maxRight);
+    }
+
+    // since this is level wise so we can just do + 1 for each level.
+    public int maxDepthIteration(TreeNode root) {
+        if (root == null) return 0;
+        int maxDepth = 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            TreeNode temp = q.remove();
+            maxDepth++;
+            for (int i = 0; i < size; i++) {
+                if (temp.left != null) q.add(temp.left);
+                if (temp.right != null) q.add(temp.right);
+            }
+        }
+        return maxDepth;
+    }
+}

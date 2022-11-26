ec7631009b9af28b40b3132bd7ec175280e246ff
YuShx01 <coveringguy0123@gmail.com>
2022-11-26 14:24:18 +0545

    added the code for the leetcode daily - maxm profit job scheduling

---
+++ src/LeetcodeDaily/MaxmProfitJobScheduling.java
@@ -0,0 +1,45 @@
+package LeetcodeDaily;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeMap;
+
+class Job {
+    int startTime;
+    int endTime;
+    int profit;
+    public Job(int startTime, int endTime, int profit) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.profit = profit;
+    }
+}
+
+
+// hard
+public class MaxmProfitJobScheduling {
+    
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        List<Job> jobs = new ArrayList<>();
+
+        for (int i = 0; i < startTime.length; i++) {
+            jobs.add(new Job(startTime[i], endTime[i], profit[i]));
+        }
+        // sorting the list on the basis of endtime.
+        Collections.sort(jobs, (x, y) -> x.endTime - y.endTime);
+        // tree map is the life saver here.
+        //      time,    profitTillTime
+        TreeMap<Integer, Integer> tmap = new TreeMap<>();
+        tmap.put(0, 0);
+        int ans = 0;
+        for (Job curr : jobs) {
+            int lastEntryTillStart = tmap.floorKey(curr.startTime);
+            int maxProfitTillStart = tmap.get(lastEntryTillStart);
+            ans = Math.max(ans, maxProfitTillStart + curr.profit);
+            tmap.put(curr.endTime, ans);
+        }
+
+        return ans;
+    }
+}
---
+++ src/LeetcodeDaily/SumOfSubarrayMins.java
@@ -0,0 +1,23 @@
+package LeetcodeDaily;
+
+// could not code the O(n) tc.
+// O(n^2) code.
+public class SumOfSubarrayMins {
+    long mod = (long) 1e9 + 7;
+    public int sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        long sum = 0;
+        for(int i = 0; i < n; i++) {
+            int min = arr[i];
+
+            for(int j = i; j < n; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                }
+                sum = (sum + min) % mod;
+            }
+        }
+
+        return (int) sum;
+}
+}

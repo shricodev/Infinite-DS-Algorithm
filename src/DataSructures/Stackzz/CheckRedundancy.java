758ef668e71b68e7984cbbf71757006d7b430ac7
YuShx01 <coveringguy0123@gmail.com>
2022-12-04 19:24:44 +0545

    added the code for the check for redundant brackets.

---
+++ src/DataSructures/Stackzz/CheckRedundancy.java
@@ -0,0 +1,36 @@
+package DataSructures.Stackzz;
+
+import java.util.Stack;
+
+public class CheckRedundancy {
+    
+    public static int checkRedundancy(String s) {
+        // code here
+        Stack<Character> st = new Stack<>();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            
+            char ch = s.charAt(i);
+
+            if (ch == '(' || ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+                st.add(ch);
+            }
+            else {
+                if (ch == ')') {
+                    boolean isRedundant = true;
+                    while (st.peek() != '(') {
+                        char top = st.peek();
+                        if (top == '*' || top == '+' || top =='-' || top == '/') {
+                            isRedundant = false;
+                        }
+                        st.pop();
+                    }
+                    // this pop is for the open bracket found.
+                    st.pop();
+                    if (isRedundant) return 1;
+                }
+            }
+        }
+        return 0;
+    }
+}

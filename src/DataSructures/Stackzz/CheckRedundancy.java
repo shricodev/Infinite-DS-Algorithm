04869ae6769a145872258eaa2d253a55e9098878
YuShx01 <coveringguy0123@gmail.com>
2022-12-05 09:54:46 +0545

    leetcode daily - dec 5

--- src/DataSructures/Stackzz/CheckRedundancy.java
+++ src/DataSructures/Stackzz/CheckRedundancy.java
@@ -3,31 +3,32 @@ package DataSructures.Stackzz;
 import java.util.Stack;
 
 public class CheckRedundancy {
-    
+
+    // the idea is if there is no operator between two parenthesis then the parenthesis is considered redundant.
     public static int checkRedundancy(String s) {
         // code here
         Stack<Character> st = new Stack<>();
         int n = s.length();
         for (int i = 0; i < n; i++) {
-            
+
             char ch = s.charAt(i);
 
             if (ch == '(' || ch == '+' || ch == '-' || ch == '*' || ch == '/') {
                 st.add(ch);
-            }
-            else {
+            } else {
                 if (ch == ')') {
                     boolean isRedundant = true;
                     while (st.peek() != '(') {
                         char top = st.peek();
-                        if (top == '*' || top == '+' || top =='-' || top == '/') {
+                        if (top == '*' || top == '+' || top == '-' || top == '/') {
                             isRedundant = false;
                         }
                         st.pop();
                     }
-                    // this pop is for the open bracket found.
+                    // this pop is for the pair open bracket found.
                     st.pop();
-                    if (isRedundant) return 1;
+                    if (isRedundant)
+                        return 1;
                 }
             }
         }
---
+++ src/LeetcodeDaily/MiddleOfLinkedList.java
@@ -0,0 +1,24 @@
+package LeetcodeDaily;
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+
+public class MiddleOfLinkedList {
+    
+    public ListNode middleNode(ListNode head) {
+        if (head.next == null) return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}

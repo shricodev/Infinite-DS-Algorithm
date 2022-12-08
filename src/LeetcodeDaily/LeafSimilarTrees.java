9d8917554457dd116d24cfbae0925533f9b377c8
YuShx01 <coveringguy0123@gmail.com>
2022-12-08 14:50:31 +0545

    leetcode daily Trees- dec 8

---
+++ src/LeetcodeDaily/LeafSimilarTrees.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeafSimilarTrees {
+    
+    public static boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> leaves1 = new ArrayList<>();
+        List<Integer> leaves2 = new ArrayList<>();
+        findLeaves(root1, leaves1);
+        findLeaves(root2, leaves2);
+        return leaves1.equals(leaves2);
+    }
+
+    public static void findLeaves(TreeNode node, List<Integer> leaves) {
+        if (node == null) return;
+
+        if (node.left == null && node.right == null) leaves.add(node.val);
+        
+        findLeaves(node.left, leaves);
+        findLeaves(node.right, leaves);
+    }
+}

f840dd77cdeb8375a7759be28c54e1ae516eef92
YuShx01 <coveringguy0123@gmail.com>
2022-12-14 18:16:22 +0545

    added the code for the siding window maximum - HARD

--- src/SlidingWindow/Fixed/MaxmOfAllSubarraysSizeK.java
+++ src/SlidingWindow/Fixed/MaxmOfAllSubarraysSizeK.java
@@ -1,26 +1,32 @@
-// package SlidingWindow.Fixed;
+package SlidingWindow.Fixed;
 
-// import java.util.ArrayList;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
-// class Solution {
-//     // Function to find maximum of each subarray of size k.
-//* NOT A WORKING CODE. */
-//     static ArrayList<Integer> max_of_subarrays(int arr[], int n, int k) {
-//         // Your code here
-//         ArrayList<Integer> ans = new ArrayList<>();
-//         int i = 0, j = 0;
-//         int maxWindow = Integer.MIN_VALUE;
-//         while (j < n) {
-//             maxWindow = Math.max(maxWindow, arr[j]);
+class Solution {
+    // question link: https://leetcode.com/problems/sliding-window-maximum/description/
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[n - k + 1]; // n - k + 1 is the no of subarrays of an array.
+        int index = 0;
+        Deque<Integer> dq = new ArrayDeque<>();
 
-//             if (j - i + 1 == k) {
-//                 ans.add(maxWindow);
-                
-//                 i++;
-//             }
-//             j++;
-//         }
-
-//         return ans;
-//     }
-// }
+        for(int i = 0; i < n; i++) {
+            // removing the out of bounds index.
+            if (!dq.isEmpty() && dq.peek() == i - k) {
+                dq.poll();
+            }
+            // removing all the smaller index in the range of k.
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
+                dq.pollLast();
+            }
+            // adding the index of the element.
+            dq.offer(i);
+            // once the window is created then add the index to the ans array.
+            if (i >= k - 1) {
+                ans[index++] = nums[dq.peek()];
+            }
+        }
+        return ans;
+    }
+}

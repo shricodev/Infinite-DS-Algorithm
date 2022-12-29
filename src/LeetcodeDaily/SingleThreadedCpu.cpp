b0aeb08e60a70bafaf46d728c2c5aa32e30efa80
YuShx01 <coveringguy0123@gmail.com>
2022-12-29 21:01:45 +0545

    leetcode daily - dec 29

---
+++ src/LeetcodeDaily/SingleThreadedCpu.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        
+        int n = tasks.size();
+
+        for(int i = 0; i < n; i++) {
+            tasks[i].push_back(i);
+        }
+
+        sort(begin(tasks), end(tasks));
+
+        vector<int> ans;
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<int>> pq; // minHeap.
+
+        long long currTime = tasks[0][0];
+        int ind = 0;
+
+        while (ind < n || !pq.empty()) {
+
+            while(ind < n && tasks[ind][0] <= currTime) {
+                pq.push({tasks[ind][1], tasks[ind][2]});
+                ind++;
+            }
+
+            pair<int, int> currTask = pq.top();
+            pq.pop();
+
+            currTime += currTask.first;
+            ans.push_back(currTask.second);
+        }
+        return ans;
+    }
+};

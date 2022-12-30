2da49fe4533aaaeb973b023fa98e0cc7f72e9cdb
YuShx01 <coveringguy0123@gmail.com>
2022-12-30 19:40:25 +0545

    added a proper soln to the problem valid parenthesis

---
+++ src/DataSructures/Stackzz/ValidParenthesisBetter.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    bool checkValidString(string s) {
+        stack<char> st;
+        unordered_map<char, int> uMap = {{'(', -1}, {'{', -2}, {'[', -3}, {')', 1}, {'}', 2}, {']', 3}};
+
+        for (char bracket : s) {
+            if (uMap[bracket] < 0) {
+                st.push(bracket);
+            } else {
+                if (st.empty())
+                    return false;
+                char c = st.top();
+                st.pop();
+                if (uMap[bracket] + uMap[c] != 0)
+                    return false;
+            }
+        }
+
+        if (st.empty())
+            return true;
+        return false;
+    }
+};

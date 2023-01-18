d86e42efacbf32c3e8d812823bb0fa10abe43de3
YuShx01 <coveringguy0123@gmail.com>
2023-01-18 13:31:34 +0545

    added optimal soln to Jan 17 - Monotonic Increasing

--- src/LeetcodeDaily/FlipStringToMonotoneInc.cpp
+++ src/LeetcodeDaily/FlipStringToMonotoneInc.cpp
@@ -2,39 +2,60 @@
 using namespace std;
 
 class Solution {
-public:
-	int n;
+  public:
+    int n;
 
-	int rec(string &s, vector<vector<int>>& dp, int currInd, int prev) {
-		if (currInd == n) return 0;
+    // not the very optimal soln
+    int rec(string &s, vector<vector<int>> &dp, int currInd, int prev) {
+        if (currInd == n)
+            return 0;
 
-		if (dp[currInd][prev] != -1) return dp[currInd][prev];
+        if (dp[currInd][prev] != -1)
+            return dp[currInd][prev];
 
-		int flip = INT_MAX;
-		int notFlip = INT_MAX;
+        int flip = INT_MAX;
+        int notFlip = INT_MAX;
 
-		if (s[currInd] == '0') {
-			if (prev == 0) {
-				flip = 1 + rec(s, dp, currInd + 1, 1);
-				notFlip = 0 + rec(s, dp, currInd + 1, 0);
-			} else { // if the prev is 1.
-				flip = 1 + rec(s, dp, currInd + 1, 1);
-			}
-		} else { // if the currInd is 1.
-			if (prev == 0) {
-				flip = 1 + rec(s, dp, currInd + 1, 0);
-				notFlip = 0 + rec(s, dp, currInd + 1, 1);
-			} else {
-				notFlip = rec(s, dp, currInd + 1, 1);
-			}
-		}
-		return dp[currInd][prev] = min(flip, notFlip);
-	}
+        if (s[currInd] == '0') {
+            if (prev == 0) {
+                flip = 1 + rec(s, dp, currInd + 1, 1);
+                notFlip = 0 + rec(s, dp, currInd + 1, 0);
+            } else { // if the prev is 1.
+                flip = 1 + rec(s, dp, currInd + 1, 1);
+            }
+        } else { // if the currInd is 1.
+            if (prev == 0) {
+                flip = 1 + rec(s, dp, currInd + 1, 0);
+                notFlip = 0 + rec(s, dp, currInd + 1, 1);
+            } else {
+                notFlip = rec(s, dp, currInd + 1, 1);
+            }
+        }
+        return dp[currInd][prev] = min(flip, notFlip);
+    }
 
     int minFlipsMonoIncr(string s) {
         n = s.length();
         vector<vector<int>> dp(n + 1, vector<int>(2, -1));
         // since it needs to be monotonic increasig so for first index wee are assuming prev as 0.
         return rec(s, dp, 0, 0);
+    }
+
+    // this is the greedy approach.
+    int minFlipsMonoIncrOpt(string s) {
+        int countOnes = 0;
+        int flips = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '1') {
+                countOnes++;
+                continue;
+            }
+            int flipped = flips + 1;
+            // since if we do not flip then we need to make all the counts of ones in the left to zero.
+            int notFlipped = countOnes;
+            flips = min(flipped, notFlipped);
+        }
+        return flips;
     }
 };

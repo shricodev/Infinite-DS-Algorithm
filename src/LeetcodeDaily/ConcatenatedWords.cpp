3dd1555f18513bc8da155a81a1a76085dea12477
YuShx01 <coveringguy0123@gmail.com>
2023-01-27 17:38:26 +0545

    Memoize the LC daily - Jan 27

--- src/LeetcodeDaily/ConcatenatedWords.cpp
+++ src/LeetcodeDaily/ConcatenatedWords.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
 class Solution {
   public:
+    // this stores that if the string is concatenated or not.
+    unordered_map<string, bool> dp;
+
     bool isConcatenated(string word, unordered_set<string> &st) {
         int n = word.length();
+
+        // if the word has been previously worked on just return it's value whether it is concatenated or not.
+        if (dp.find(word) != dp.end())
+            return dp[word];
 
         for (int i = 0; i < n; i++) {
             string prefix = word.substr(0, i + 1);
@@ -12,14 +19,15 @@ class Solution {
 
             // if the suffix and prefix are both in the array in 2 pieces.    if the prefix is in one piece and suffix is in multiple pieces.
             if (st.find(prefix) != st.end() && st.find(suffix) != st.end() || st.find(prefix) != st.end() && isConcatenated(suffix, st)) {
-                return true;
+                return dp[word] = true;
             }
         }
-        return false;
+        return dp[word] = false;
     }
 
     vector<string> findAllConcatenatedWordsInADict(vector<string> &words) {
         int n = words.size();
+        dp.clear();
         unordered_set<string> st(begin(words), end(words));
         vector<string> ans;
         // iterate over each word and check if it is concatenated.

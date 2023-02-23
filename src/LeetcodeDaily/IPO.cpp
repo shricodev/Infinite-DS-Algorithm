ff2573ea8cf5e7eb7e6d66db9f36a4312b10639e
Shrijal Acharya <coveringguy0123@gmail.com>
2023-02-23 18:16:11 +0545

    LeetCode Daily GREEDY - Feb 23

---
+++ src/LeetcodeDaily/IPO.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int n = profits.size();
+        vector<pair<int, int>> projects;
+        for (int i = 0; i < n; i++) {
+            projects.emplace_back(capital[i], profits[i]);
+        }
+        sort(projects.begin(), projects.end());
+        priority_queue<int> priority;
+        int j = 0;
+        for (int i = 0; i < k; i++) {
+            while (j < n && projects[j].first <= w) {
+                priority.push(projects[j++].second);
+            }
+            if (priority.empty()) {
+                break;
+            }
+            w += priority.top();
+            priority.pop();
+        }
+        return w;
+    }
+};
